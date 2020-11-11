63f0a57f6129d01ca4400b0f8f536900ce19fcbf
Emiliano <russo.emiliano97@gmail.com>
2020-11-11 13:39:23 -0300

    Refactor al metodo de la clase

--- ObligatorioDDA2/BusinessLogic/Models/Entidades/Logica ReporteA/Logica_ReporteA.cs
+++ ObligatorioDDA2/BusinessLogic/Models/Entidades/Logica ReporteA/Logica_ReporteA.cs
@@ -10,14 +10,19 @@ namespace BusinessLogic.Models.Entidades.Logica_ReporteA
     {
         public List<Hotel_CantReservas> GetReporteA(InfoReporte info)
         {
-            List<Hotel_CantReservas> lista_retorno = new List<Hotel_CantReservas>();
             PuntoTuristico punto = new PuntoTuristico
             {
                 Nombre = info.NombrePunto
             };
             List<Alojamiento> lista_alojamientos = Sistema.GetInstancia().repo.GetAlojamientos(punto);
+            return Analizar_alojamientos(lista_alojamientos,info);
+        }
+
+        private List<Hotel_CantReservas> Analizar_alojamientos(List<Alojamiento> lista_alojamientos, InfoReporte info)
+        {
+            List<Hotel_CantReservas> lista_retorno = new List<Hotel_CantReservas>();
             foreach (var alojamiento in lista_alojamientos)
-            {
+            {              
                 Hotel_CantReservas hc = new Hotel_CantReservas
                 {
                     CantidadReservas = Sistema.GetInstancia().repo.GetReservasValidas(info).Count,
