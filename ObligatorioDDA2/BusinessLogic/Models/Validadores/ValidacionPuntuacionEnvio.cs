55f126323aac8a575b45f176b02ea5ccadf03ac7
Emiliano <russo.emiliano97@gmail.com>
2020-11-11 21:53:51 -0300

    Test para Puntuacion

---
+++ ObligatorioDDA2/BusinessLogic/Models/Validadores/ValidacionPuntuacionEnvio.cs
@@ -0,0 +1,36 @@
+﻿using BusinessLogic.Models.Entidades;
+using ObligatorioDDA2.Models.Interfaces;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace BusinessLogic.Models.Validadores
+{
+    public class ValidacionPuntuacionEnvio : Validacion<Puntuacion_Recibir>
+    {
+        protected override void Existencia(Puntuacion_Recibir objeto)
+        {
+            throw new NotImplementedException();
+        }
+
+        protected override void Existencia(string key)
+        {
+            throw new NotImplementedException();
+        }
+
+        protected override void NulosVacios(Puntuacion_Recibir objeto)
+        {
+            throw new NotImplementedException();
+        }
+
+        protected override void ObjetosDentro(Puntuacion_Recibir objeto)
+        {
+            throw new NotImplementedException();
+        }
+
+        protected override void Unicidad(Puntuacion_Recibir objeto)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
