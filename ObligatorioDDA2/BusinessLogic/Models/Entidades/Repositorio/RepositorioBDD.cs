6e967a179a8b087d8b7b75a8f0db23e6d99db84b
Emiliano <russo.emiliano97@gmail.com>
2020-11-12 13:28:52 -0300

    Puntos de entrada añadidos

--- ObligatorioDDA2/BusinessLogic/Models/Entidades/Repositorio/RepositorioBDD.cs
+++ ObligatorioDDA2/BusinessLogic/Models/Entidades/Repositorio/RepositorioBDD.cs
@@ -159,7 +159,9 @@ namespace ObligatorioDDA2.Models.Entidades.Repositorio
                 {
                     Codigo = p.Reserva.Codigo,
                     Comentario = p.Comentario,
-                    Puntos = p.Puntos
+                    Puntos = p.Puntos,
+                    Nombre = p.Reserva.InfoReserva.Nombre,
+                    Apellido = p.Reserva.InfoReserva.Apellido
                 };
                 lista_retorno.Add(pun);
             }
